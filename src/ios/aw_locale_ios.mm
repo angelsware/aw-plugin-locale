efce7cf716b14eebba85672fa0636a0b9446459b
Joachim Klahr <joachim.klahr@gmail.com>
2020-10-07 13:57:55 +0200

    Fixed bug when returning language on iOS.

--- src/ios/aw_locale_ios.mm
+++ src/ios/aw_locale_ios.mm
@@ -3,20 +3,22 @@
 
 namespace Locale {
 	bool CDevice::getLanguage(char* buffer) {
-        NSString *language = [[NSLocale preferredLanguages] objectAtIndex:0];
-        const char* lang = [language UTF8String];
-        strcpy(buffer, lang);
+		NSString *language = [[NSLocale preferredLanguages] firstObject];
+		NSDictionary *languageDic = [NSLocale componentsFromLocaleIdentifier:language];
+		NSString *languageCode = [languageDic objectForKey:@"kCFLocaleLanguageCodeKey"];
+		const char* lang = [languageCode UTF8String];
+		strcpy(buffer, lang);
 		return true;
 	}
 
 	bool CDevice::getCountry(char* buffer) {
-        NSLocale *currentLocale = [NSLocale currentLocale];
-        NSString *countryCode = [currentLocale objectForKey:NSLocaleCountryCode];
-        if (countryCode != nil) {
-            const char* lang = [countryCode UTF8String];
-            strcpy(buffer, lang);
-            return true;
-        }
-        return false;
+		NSLocale *currentLocale = [NSLocale currentLocale];
+		NSString *countryCode = [currentLocale objectForKey:NSLocaleCountryCode];
+		if (countryCode != nil) {
+			const char* lang = [countryCode UTF8String];
+			strcpy(buffer, lang);
+			return true;
+		}
+		return false;
 	}
 }
